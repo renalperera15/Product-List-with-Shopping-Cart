8eca5cc442fab8de54c04f55856b77cc783dc978
Renal Perera <renal.p@informaticsint.com>
2026-03-06 22:37:50 +0530

    Added the file structure

---
+++ js/script.js
@@ -0,0 +1,43 @@
+let cart = []
+let total = 0
+
+function addToCart(name,price){
+
+cart.push({name,price})
+total += price
+
+displayCart()
+
+}
+
+function displayCart(){
+
+let cartDiv = document.getElementById("cart")
+cartDiv.innerHTML=""
+
+cart.forEach((item,index)=>{
+
+let div = document.createElement("div")
+
+div.innerHTML = `
+${item.name} - $${item.price}
+<button onclick="removeItem(${index})">Remove</button>
+`
+
+cartDiv.appendChild(div)
+
+})
+
+document.getElementById("total").innerText = total
+
+}
+
+function removeItem(index){
+
+total -= cart[index].price
+
+cart.splice(index,1)
+
+displayCart()
+
+}
