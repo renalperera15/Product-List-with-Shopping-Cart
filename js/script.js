22685345ff0d1c59651f5647ad8b1306c4576d29
Renal Perera <renal.p@informaticsint.com>
2026-03-09 10:20:33 +0530

    Updated the css file

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 let cart = []
 let total = 0
 
-function addToCart(name,price){
+function addToCart(name, price){
 
-cart.push({name,price})
+cart.push({name, price})
+
 total += price
 
 displayCart()
@@ -13,12 +14,14 @@ displayCart()
 function displayCart(){
 
 let cartDiv = document.getElementById("cart")
-cartDiv.innerHTML=""
 
-cart.forEach((item,index)=>{
+cartDiv.innerHTML = ""
+
+cart.forEach((item, index)=>{
 
 let div = document.createElement("div")
 
+div.classList.add("cart-item")
 div.innerHTML = `
 ${item.name} - $${item.price}
 <button onclick="removeItem(${index})">Remove</button>
